e2fc7532041f59f039fcec41f067be32356a762a
okyryche <olexiy.kyrychenko@gmail.com>
2016-11-09 11:20:55 +0100

    added logging

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 
 include 'vendor/autoload.php';
 
-$app = new \Slim\App();
+$container = new \Slim\Container();
 
-$app->any('/', function ($request, $response, $args) {
+$container['logger'] = function ($c) {
+    $logger         = new Monolog\Logger('logger');
+    $filename       = __DIR__ . '/access.log';
+    $stream         = new Monolog\Handler\StreamHandler($filename, Monolog\Logger::DEBUG);
+    $fingersCrossed = new Monolog\Handler\FingersCrossedHandler(
+        $stream, Monolog\Logger::DEBUG);
+    $logger->pushHandler($fingersCrossed);
+
+    return $logger;
+};
+
+$app = new \Slim\App($container);
+
+// logging middleware
+$app->add(function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, $next) {
+    /** @var \Psr\Log\LoggerInterface $logger */
+    $logger = $this->logger;
+    $logger->info("Request", ["route" => $request->getUri()->getPath(), "method" => $request->getMethod(), "params" => $request->getParams()]);
+    $response = $next($request, $response);
+    $logger->info("Response", ["code" => $response]);
+    return $response;
+});
+
+$app->any('/', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, $args) {
     $holidayChecker = new \Checkdomain\Holiday\Util();
     $client         = new \GuzzleHttp\Client();
     $result         = (new \Olek\Ifttt\Leave($holidayChecker, $client))->run();
